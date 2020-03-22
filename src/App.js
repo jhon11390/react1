aeb87b73fa18bacc1e0665bc9ba5942195e5fc26
jhon11390 <meljhon2@gmail.com>
2020-03-21 21:42:48 -0500

    Se crea primera aplicacion con react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Hola mundo</h1>
+    <div>
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+    </div>
   );
 }
 
